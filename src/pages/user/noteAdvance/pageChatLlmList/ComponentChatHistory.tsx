c385332c2e6d4c2a46a9c1bf55820929456692c9
Nithin Bhandari <nithinibhandari2017@gmail.com>
2025-04-24 22:40:55 +0530

    Add 'New Chat' button to initiate a new chat session in chat history component

--- src/pages/user/noteAdvance/pageChatLlmList/ComponentChatHistory.tsx
+++ src/pages/user/noteAdvance/pageChatLlmList/ComponentChatHistory.tsx
@@ -86,6 +86,19 @@ const ComponentChatHistory = () => {
                 Chat History
             </h2>
 
+            {/* chat new */}
+            <div className="mb-4">
+                <button
+                    className="bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600 transition-colors duration-150"
+                    onClick={() => {
+                        alert('New chat initiated!');
+                    }}
+                >
+                    New Chat
+                </button>
+            </div>
+
+            {/* search */}
             <div className="mb-4">
                 <DebounceInput
                     debounceTimeout={750}
