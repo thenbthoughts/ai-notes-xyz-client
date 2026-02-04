991b2fedc3e82fa7e10c98756840504c44da8f8f
Nithin I Bhandari <nithinibhandari2017@gmail.com>
2026-02-04 22:17:50 +0530

    Remove link to Model Preferences in SettingHeader for streamlined user settings navigation

--- src/pages/user/settings/SettingHeader.tsx
+++ src/pages/user/settings/SettingHeader.tsx
@@ -11,7 +11,6 @@ const SettingHeader = () => {
 
             <div className="mb-5">
                 <Link to="/user/setting/" className="text-white hover:underline mr-2">Profile Settings</Link>{'|'}
-                <Link to="/user/setting/model-preference" className="text-white hover:underline mx-2">Model Preferences</Link>{'|'}
                 <Link to="/user/setting/ollama-models" className="text-white hover:underline mx-2">Ollama Models</Link>{'|'}
                 <Link to="/user/setting/login-history" className="text-white hover:underline mx-2">Login History</Link>{'|'}
                 <Link to="/user/setting/api-key" className="text-white hover:underline mx-2">API Keys</Link> {'|'}
